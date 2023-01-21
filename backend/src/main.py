622ac79a06cf839730f647e3d0107384213d7e94
Matias Borghi <borghi.matias@gmail.com>
2023-01-21 19:57:05 -0300

    add connection to external API using http3 in FastAPI

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,6 +3,11 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import http3
+import json
+
+client = http3.AsyncClient()
+
 app = FastAPI()
 
 origins = [
@@ -26,3 +31,23 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/rates/{rate_id}")
+async def read_rate(rate_id: str):
+    """ Call the external API of Coincap the gets the current rate of a given coin.
+
+    Args:
+        rate_id (str): name of the coin to get the current rate.
+
+    Returns:
+        id:	unique identifier for asset or fiat
+        symbol:	most common symbol used to identify asset or fiat
+        currencySymbol:	currency symbol used to identify asset or fiat
+        rateUsd: rate conversion to USD
+        type: type of currency - fiat or crypto
+    """
+    url = f"https://api.coincap.io/v2/rates/{rate_id}"
+    print(url)
+    r = await client.get(url)
+    return json.loads(r.text)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,61 @@ import { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ["repoData"],
+  const {
+    isLoading: loadingItem,
+    error: errorItem,
+    data: item,
+  } = useQuery({
+    queryKey: ["item"],
     queryFn: () =>
       fetch("http://0.0.0.0:8080/items/1?q=another").then((res) => res.json()),
   });
 
+  const {
+    isLoading: loadingRate,
+    error: errorRate,
+    data: rate,
+  } = useQuery({
+    queryKey: ["rate"],
+    queryFn: () =>
+      fetch(`http://0.0.0.0:8080/rates/bitcoin`).then((res) => res.json()),
+  });
+
   useEffect(() => {
-    if (!data) return;
-    console.log(data);
-  }, [data]);
+    if (item) {
+      console.log(item);
+    }
+    if (rate) {
+      console.log(rate.data);
+    }
+  }, [item, rate]);
 
-  if (isLoading) return "Loading...";
+  if (loadingItem || loadingRate) return <h1>"Loading..."</h1>;
 
-  if (error) return "An error has occurred: " + error;
+  if (errorItem) return <h1>{"An error has occurred: " + errorItem}</h1>;
 
-  return <h1>Testing</h1>;
+  if (errorRate) return <h1>{"An error has occurred: " + errorRate}</h1>;
+
+  return (
+    <div>
+      <h1>Testing</h1>
+      {rate.data && (
+        <>
+          <h3>Reading from external API</h3>
+          <table>
+            <tr>
+              <th>Symbol</th>
+              <th>Rate</th>
+            </tr>
+            <tr>
+              <td>{rate.data.symbol}</td>
+              <td>{parseFloat(rate.data.rateUsd).toFixed(0)}</td>
+            </tr>
+          </table>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
